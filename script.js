4ede299e91e8c4dab783c25778d7b83cf21a2322
phuctranvannguyen-cyber <phuc.tranvannguyen@nbs.edu.vn>
2026-07-29 17:04:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const screen = document.getElementById("screen");
+
+const lines = [
+
+    "[BOOT] Li Xun System v1.0",
+    "",
+    "[OK] Loading kernel...",
+    "[OK] Initializing memory...",
+    "[OK] Checking graphics...",
+    "[OK] Starting rendering engine...",
+    "[OK] Establishing secure connection...",
+    "",
+    "[SYS] Compiling...",
+    "",
+    "[DONE] Build Successful."
+
+];
+
+let line = 0;
+let char = 0;
+
+function typeWriter(){
+
+    if(line >= lines.length){
+
+        screen.innerHTML +=
+        '<span class="cursor"></span>';
+
+        return;
+
+    }
+
+    const current = lines[line];
+
+    if(char < current.length){
+
+        screen.innerHTML += current.charAt(char);
+
+        char++;
+
+        setTimeout(typeWriter,35);
+
+    }else{
+
+        screen.innerHTML += "<br>";
+
+        line++;
+
+        char = 0;
+
+        setTimeout(typeWriter,250);
+
+    }
+
+}
+
+typeWriter();
